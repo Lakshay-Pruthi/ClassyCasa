5310cb13c1fe585ca97c1ffaed72ba7fc4d2ec1b
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-08-07 00:01:49 +0530

    deployement finalized

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   base: "/ClassyCasa/",
   server: {
     proxy: {
-      "/api/": "http://localhost:5000/",
+      "/api": "http://localhost:5000/",
     },
   },
 });
