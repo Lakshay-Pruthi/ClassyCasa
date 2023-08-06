061d718506bee040bb42337f2675d2863d764f49
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-08-06 17:24:07 +0530

    deployement finalized

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ dotenv.config();
 import { connectToDatabase } from "./db/connectToDatabase.js";
 connectToDatabase();
 
-app.use(express.static(path.join(__dirname, "../Client/dist/index.html")));
+app.use(express.static(path.join(__dirname, "./Client/dist/index.html")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./Client/dist/index.html"));
 });
 
 const port = process.env.PORT || 8000;
