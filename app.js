d48308da5c5c426ac09c8b55adc5b24cedc315ea
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-08-06 17:32:52 +0530

    deployement finalized

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 import router from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Dotenv
 import dotenv from "dotenv";
