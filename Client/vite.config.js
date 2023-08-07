929df2784216447dc168a75cf204f1254d2714ee
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-08-07 12:35:23 +0530

    deployement finalized

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ClassyCasa/",
+  base: "/",
   server: {
     proxy: {
-      "/api": "http://localhost:5000/",
+      "/api/": "http://localhost:5000/",
     },
   },
 });
