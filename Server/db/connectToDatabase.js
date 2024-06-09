e34edf4af549728f68d168f9f63b499589907681
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2024-06-09 15:51:15 +0530

    file structure updated

---
+++ Server/db/connectToDatabase.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const Db = process.env.MONGO;
+
+export async function connectToDatabase() {
+  try {
+    await mongoose.connect(Db);
+    console.log("Mongo Db connected");
+  } catch (err) {
+    console.log("Error occured");
+  }
+}
